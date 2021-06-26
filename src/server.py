be8ba04fe6096a61bd68124314d0c123702dff50
roshan <mailme.roshanbhattarai@gmail.com>
2021-06-26 16:54:07 +0545

    Fixed port forwarding docker

--- src/server.py
+++ src/server.py
@@ -2,5 +2,5 @@ import uvicorn
 from app import request_handler
 
 if __name__ == "__main__":
-    uvicorn.run("server:request_handler", host="127.0.0.1",
+    uvicorn.run("server:request_handler", host="0.0.0.0",
                 port=5000, log_level="info", reload=True)
